f69c1438f6c8ba4b6b7426d151ac0953da2036ed
Adriel Santos <adriel@adriel.eu>
2025-03-29 12:39:21 -0300

    String java mimic

--- src/ast/TypeReference.js
+++ src/ast/TypeReference.js
@@ -1,40 +1,93 @@
-/*
- * Copyright 2002-2019 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import {SpelNode} from './SpelNode';
-
-/**
- * Represents a reference to a type, for example
- * {@code "T(String)" or "T(com.somewhere.Foo)"}.
- *
- * @author Andy Clement
- */
-function createNode(position, node, _dims) {
-    var node = SpelNode.create('typeref', position, node);
-
-    node.getValue = function (state) {
-        throw {
-            name: 'MethodNotImplementedException',
-            message: 'TypeReference: Not implemented'
+const javaString = {
+    /**
+     * Mimics:
+     * public static String join(CharSequence delimiter, CharSequence... elements)
+     * public static String join(CharSequence delimiter, Iterable<? extends CharSequence> elements)
+     */
+    join: function(delimiter, elements) {
+        if (Array.isArray(elements)) {
+            return elements.join(delimiter);
+        } else if (elements != null && typeof elements[Symbol.iterator] === 'function') {
+            return Array.from(elements).join(delimiter);
         }
-    };
+        throw new Error('join: elements is not iterable');
+    },
 
-    return node;
-}
+    /**
+     * Mimics:
+     * public static String format(String format, Object... args)
+     * (A very simple implementation that replaces %s with the provided arguments.)
+     */
+    format: function(format, ...args) {
+        let i = 0;
+        return format.replace(/%s/g, function() {
+            return args[i++];
+        });
+    },
 
-export var TypeReference =  {
-    create: createNode
+    /**
+     * Mimics:
+     * public static String valueOf(Object obj)
+     * (Also covers many of the overloaded valueOf methods by simply converting to a string.)
+     */
+    valueOf: function(arg) {
+        return String(arg);
+    },
+
+    /**
+     * Mimics:
+     * public static String copyValueOf(char[] data)
+     * public static String copyValueOf(char[] data, int offset, int count)
+     */
+    copyValueOf: function(chars, offset, count) {
+        if (!Array.isArray(chars)) {
+            throw new Error('copyValueOf: first argument must be an array of characters');
+        }
+        if (offset === undefined || count === undefined) {
+            return chars.join('');
+        }
+        return chars.slice(offset, offset + count).join('');
+    },
+
+    /**
+     * Mimics the static field:
+     * public static final Comparator<String> CASE_INSENSITIVE_ORDER
+     */
+    CASE_INSENSITIVE_ORDER: {
+        compare: function(s1, s2) {
+            s1 = String(s1).toLowerCase();
+            s2 = String(s2).toLowerCase();
+            if (s1 < s2) return -1;
+            if (s1 > s2) return 1;
+            return 0;
+        }
+    }
+};
+
+// Create a single mapping for both type references.
+const typeMapping = {
+    "String": javaString,
+    "java.lang.String": javaString
+};
+
+export var TypeReference = {
+    create: function(position, typeNode, _dims) {
+        // Create the node as usual.
+        let node = SpelNode.create('typeref', position, typeNode);
+
+        // Override getValue to resolve the type reference using our mapping.
+        node.getValue = function(state) {
+            // Assume the first child node returns the type name as a string.
+            let typeName = node.children[0].getValue(state);
+            if (typeMapping.hasOwnProperty(typeName)) {
+                return typeMapping[typeName];
+            }
+            throw {
+                name: 'TypeReferenceException',
+                message: 'No mapping found for type: ' + typeName
+            };
+        };
+
+        return node;
+    }
 };
